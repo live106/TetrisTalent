625a9e5aac89c85d33978c371ea83392949f5ddf
live106 <269504827@qq.com>
2015-07-24 11:01:23 +0800

    remove Chinese comment for encoding reason

--- src/nodes/effect/Explosion.js
+++ src/nodes/effect/Explosion.js
@@ -1,35 +1,3 @@
-/****************************************************************************
- Cocos2d-html5 show case : Moon Warriors
-
- Copyright (c) 2011-2012 cocos2d-x.org
- Copyright (c) 2013-2014 Chukong Technologies Inc.
-
- http://www.cocos2d-x.org
-
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
-
- @Authors:
- Programmer: Shengxiang Chen (陈升�?), Dingping Lv (吕定�?), Ricardo Quesada
- Effects animation: Hao Wu (吴昊)
- Quality Assurance: Sean Lin (林顺)
- ****************************************************************************/
-
 var Explosion = cc.Sprite.extend({
     tmpWidth:0,
     tmpHeight:0,
